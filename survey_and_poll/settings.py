02f78b8cd207b89b8f359a7625c135ce5992bd7e
jaymine <okoegualebenjamin38@gmail.com>
2025-11-27 03:07:22 +0100

    Adding apps to INSTALLED_APPS

--- survey_and_poll/settings.py
+++ survey_and_poll/settings.py
@@ -37,6 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Local apps
+    'authentication',
+    'polls',
+    'users',
+
+    # Third-party apps
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
