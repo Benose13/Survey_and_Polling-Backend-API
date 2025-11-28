bdf2bb8d6e79f13953f28d955d9cb8204626d055
jaymine <okoegualebenjamin38@gmail.com>
2025-11-28 04:38:31 +0100

    Expand app to handle users profiles, roles, etc.

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework import generics
-from django.contrib.auth.models import User
 from .serializers import RegisterSerializer
 from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.serializers import ModelSerializer
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
